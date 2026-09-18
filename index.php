019cd8f759bc9253cfd47e79684c2244109570c9
Alexita <125055470@upq.edu.mx>
2026-09-18 17:06:58 -0600

    se trabajaron estilos en php

---
+++ index.php
@@ -0,0 +1,41 @@
+<!-- <?php
+echo "Holi boli!";
+?>
+ -->
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1{
+            color: red;
+        }
+        h2{
+            color: orange;
+        }
+        h3{
+            color: yellow;
+        }
+        h4{
+            color: green;
+        }
+        h5{
+            color: blue;
+        }
+        h6{
+            color: purple;
+        }
+    </style>
+ </head>
+ <body>
+    <h1> hola crayola</h1>
+    <h2>hola crayola</h2>
+    <h3>hola crayola</h3>
+    <h4>hola crayola</h4>
+    <h5>hola crayola</h5>
+    <h6>hola crayola</h6>
+ </body>
+
+ </html>
